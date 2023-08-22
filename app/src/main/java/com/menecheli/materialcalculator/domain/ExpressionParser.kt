e07fa9910689e7538167fd0ef446374385d18bcc
Menecheli <diego.menecheli@teapayment.co.uk>
2023-08-21 21:46:16 -0300

    add: expression parts

---
+++ app/src/main/java/com/menecheli/materialcalculator/domain/ExpressionParser.kt
@@ -0,0 +1,10 @@
+package com.menecheli.materialcalculator.domain
+
+class ExpressionParser(
+    private val calculation: String
+) {
+
+    fun parse(): List<ExpressionPart> {
+        return emptyList()
+    }
+}

6688f8999d92b3398cbf260eb57e6f48ff38a0f2
Menecheli <diego.menecheli@teapayment.co.uk>
2023-08-21 22:37:30 -0300

    add: parser imp

--- app/src/main/java/com/menecheli/materialcalculator/domain/ExpressionParser.kt
+++ app/src/main/java/com/menecheli/materialcalculator/domain/ExpressionParser.kt
@@ -5,6 +5,53 @@ class ExpressionParser(
 ) {
 
     fun parse(): List<ExpressionPart> {
-        return emptyList()
+        val result = mutableListOf<ExpressionPart>()
+
+        var i = 0
+        while (i < calculation.length) {
+            val curChar = calculation[i]
+            when {
+                curChar in operationSymbols -> {
+                    result.add(
+                        ExpressionPart.Op(operationFromSymbols(curChar))
+                    )
+                }
+
+                curChar.isDigit() -> {
+                    i = parseNumber(i, result)
+                    continue
+                }
+
+                curChar in "()" -> {
+                    parseParentheses(curChar, result)
+                }
+            }
+            i++
+        }
+        return result
+    }
+
+    private fun parseNumber(startingIndex: Int, result: MutableList<ExpressionPart>): Int {
+        var i = startingIndex
+        val numberAsString = buildString {
+            while (i < calculation.length && calculation[i] in "0123456789.") {
+                append(calculation[i])
+                i++
+            }
+        }
+        result.add(ExpressionPart.Number(numberAsString.toDouble()))
+        return i
+    }
+
+    private fun parseParentheses(curChar: Char, result: MutableList<ExpressionPart>) {
+        result.add(
+            ExpressionPart.Parentheses(
+                tye = when (curChar) {
+                    '(' -> ParenthesesTye.Opening
+                    ')' -> ParenthesesTye.Closing
+                    else -> throw IllegalArgumentException("Invalid parentheses type")
+                }
+            )
+        )
     }
 }
